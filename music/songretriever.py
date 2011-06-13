269d8173524196c739827df29fa4ca184e4c9215
Jeremy Newton <alexjnewt@hotmail.com>
2011-06-13 15:47:31 -0400

    Updated the format help (2.11.6-dev)
    I commited the help into emesene 2.11.6-dev, this just makes it so it works.

--- music/songretriever.py
+++ music/songretriever.py
@@ -37,7 +37,7 @@ class BaseMusicHandlerConfig(BaseTable):
         BaseTable.__init__(self, 4, 1)
         self.config = config
 
-        self.append_entry_default(_('Message Format: '), 'format', \
+        self.append_entry_default(_('Message Format: '), 'music', \
                                  'config.music_format', config.music_format)
         self.append_check(_('Use the album art as an avatar'), 'config.change_avatar')
         self.append_markup(_('See the "listening to" option in the extentions tab of the'))
