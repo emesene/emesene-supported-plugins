5e9120cf58a6dee468ce9525885b7559bd4c5281
Jose Rostagno <joserostagno@vijona.com.ar>
2011-12-16 12:07:09 -0300

    Music plugin: Implement automatic updates for Banshee refs #51

--- music/handler_banshee.py
+++ music/handler_banshee.py
@@ -12,6 +12,31 @@ class BansheeHandler(DBusBase.DBusBase):
                  iface_name = 'org.bansheeproject.Banshee',
                  iface_path = '/org/bansheeproject/Banshee/PlayerEngine'):
         DBusBase.DBusBase.__init__(self, main_window, iface_name, iface_path)
+        self.check_song()
+
+    def state_changed(self, state):
+        '''process player status changes'''
+        if state in ["idle", "loading", "loaded"]:
+            #skip intermediate status
+            return
+        self.check_song()
+
+    def reconnect(self):
+        '''method to attemp a reconnection, via dbus, this is only
+        called if the bus object is not initialized'''
+        if DBusBase.DBusBase.reconnect(self):
+            self.dbuspropiface = self.module.Interface(self.iface,
+                                    dbus_interface='org.bansheeproject.Banshee.PlayerEngine')
+            self.dbuspropiface.connect_to_signal('StateChanged', self.state_changed)
+            return True
+
+        return False
+
+    def get_automatic_updates(self):
+        '''When the handler can do automatic updates of player status
+           and timeout are not needed.
+        '''
+        return True
 
     def is_playing(self):
         '''Returns True if a song is being played'''
