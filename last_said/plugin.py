8e00d53077b3dcf05c00c6f9aa55c19b0ac7264a
Jose Rostagno <joserostagno@vijona.com.ar>
2012-01-05 15:43:23 -0300

    LastSaid: Use new get_conversation API

--- last_said/plugin.py
+++ last_said/plugin.py
@@ -19,8 +19,9 @@
 import extension
 from plugin_base import PluginBase
 
-import LastSaid
-
+import e3
+import gui
+import datetime
 import Preferences
 
 class Plugin(PluginBase):
@@ -31,11 +32,21 @@ class Plugin(PluginBase):
         PluginBase.__init__(self)
 
     def start(self, session):
-        self.extensions_register()
+        self.session = session
+        self.conversations = []
+        self.limit = self.session.config.get_or_set('i_last_said_messages', 5)
+        self.session.conversation_start_locked = True
+        self.session.signals.conv_started.subscribe(
+            self._on_started_message)
         return True
 
     def stop(self):
         #disconect signals
+        self.session.signals.conv_started.unsubscribe(
+            self._on_started_message)
+        self.session.conversation_start_locked = False
+        self.session = None
+        self.conversations = []
         return False
 
     def config(self, session):
@@ -46,5 +57,45 @@ class Plugin(PluginBase):
         '''this plugin is configurable'''
         return True
 
-    def extensions_register(self):
-        extension.register('after_new_conversation', LastSaid.LastSaid)
+    def _on_started_message(self, cid, members):
+        conversation = self.session.get_conversation(cid)
+        self.conversations.append(conversation)
+        if not (conversation.members[0] is None or conversation.is_group_chat):
+            dest = self.session.contacts.me.account
+            src = conversation.members[0]
+            self.session.logger.get_chats(src, dest, self.limit, self._on_chats_ready)
+        else:
+            conversation.conv_status.clear()
+            conversation.output.unlock ()
+
+    def _on_chats_ready(self, results):
+        '''called when the chat history is ready'''
+        conversation = self.conversations.pop()
+        conversation.conv_status.clear()
+
+        if not results:
+            conversation.output.unlock ()
+            return
+
+        for stat, timestamp, msg_text, nick, account in results:
+            is_me = self.session.contacts.me.account == account
+            if is_me:
+                contact = self.session.contacts.me
+            else:
+                contact = self.session.contacts.get(account)
+
+            if contact == None:
+                contact = e3.Contact(account, nick=nick)
+
+            datetimestamp = datetime.datetime.utcfromtimestamp(timestamp)
+            message = e3.Message(e3.Message.TYPE_OLDMSG, msg_text,
+                        account, timestamp=datetimestamp)
+            message.style = conversation.cstyle
+
+            if is_me:
+                conversation.output_message(message, None)
+            else:
+                conversation.input_message(message, contact, None, None)
+
+        conversation.conv_status.clear()
+        conversation.output.unlock ()
