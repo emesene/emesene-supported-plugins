5e8c624c7a9179ac869845fd9495c2ba25e1ea90
Sven (Sbte) <svenb.linux@gmail.com>
2012-01-14 20:58:27 +0100

    music: Actually return a correct version value
    Closes #59

--- music/plugin.py
+++ music/plugin.py
@@ -66,7 +66,9 @@ class Plugin(PluginBase):
         split_version.reverse()
         value = 0
         for i, val in enumerate(split_version):
-            value += (int(val) << ((3-i) * 8))
+            value += (int(val) << (i * 8))
+
+        return value
 
     def category_register(self):
         import songretriever
